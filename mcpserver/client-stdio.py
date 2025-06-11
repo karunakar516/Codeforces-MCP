44cf685f0a0494c870c0a40639f72409e344e907
Karunakar Balivada <karunakarbalivada786@gmail.com>
2025-06-12 04:44:49 +0530

    added client for stdio transport type

--- mcpserver/client-stdio.py
+++ mcpserver/client-stdio.py
@@ -1,24 +1,17 @@
 import asyncio
 import nest_asyncio
-from mcp import ClientSession
-from mcp.client.sse import sse_client
-
-nest_asyncio.apply()  # Needed to run interactive python
-
-"""
-Make sure:
-1. The server is running before running this script.
-2. The server is configured to use SSE transport.
-3. The server is listening on port 8050.
-
-To run the server:
-uv run server.py
-"""
-
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
 
 async def main():
-    # Connect to the server using SSE
-    async with sse_client("http://localhost:8000/sse") as (read_stream, write_stream):
+    # Define server parameters
+    server_params = StdioServerParameters(
+        command="python",  # The command to run your server
+        args=["app.py"],  # Arguments to the command
+    )
+
+    # Connect to the server
+    async with stdio_client(server_params) as (read_stream, write_stream):
         async with ClientSession(read_stream, write_stream) as session:
             # Initialize the connection
             await session.initialize()
@@ -29,7 +22,7 @@ async def main():
             for tool in tools_result.tools:
                 print(f"  - {tool.name}: {tool.description}")
 
-            # Call our Codeforces profile tool
+            # Call our Codeforces profile Tool
             result = await session.call_tool("get_user_profiles", arguments={"handles":"tourist,karunakar_516"})
             print(f"Their Codeforces profiles are = {result.content[0].text}")
 
